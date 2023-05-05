91929c7f99e9eb42c6c5cc8576f1f917fb016dd9
swarajbari18 <131468935+swarajbari18@users.noreply.github.com>
2023-05-05 19:54:56 +0530

    Update _2_Mirror_Magic.py

--- _2_Mirror_Magic.py
+++ _2_Mirror_Magic.py
@@ -4,3 +4,13 @@ for i in str_inp :
     stringsss = i + stringsss
 
 print('\nThe Mirror of the string is:  ', stringsss)
+
+
+
+
+# Second Program based on slicing
+# 
+#str_inp = input('Enter the string    ')
+#reversed = str_inp[-1::-1]
+#print(reversed)
+#
