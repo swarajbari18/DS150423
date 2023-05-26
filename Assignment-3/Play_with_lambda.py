1d9d943512fc879274626b34b305eea2d5d9174d
swarajbari18 <131468935+swarajbari18@users.noreply.github.com>
2023-05-26 16:04:36 +0530

    Delete Assignment-3 directory

--- Assignment-3/Play_with_lambda.py
+++
@@ -1,4 +0,0 @@
-sample_input = float(input('Enter the sample input:   '))
-lam_func = lambda x : x+25
-sample_output = lam_func(sample_input)
-print(f'Sample input: {sample_input}\nSample output: {sample_output}')
